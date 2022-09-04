dfa95db1e08411d95aee5b830844b25f71eca56f
Nigel Harsch <nigelharsch@gmail.com>
2022-09-04 00:40:10 -0700

    I think echoing works

--- cqrs-web-services/project.clj
+++ cqrs-web-services/project.clj
@@ -1,12 +1,18 @@
 (defproject cwars-web-services "0.1.0-SNAPSHOT"
   :description "web service layer for a CQRS app"
-  :plugins [[lein-ring "0.12.6"]]
-  :ring {:handler cqrs-web-services.core.endpoints/handler}
+  :plugins [[lein-ring "0.12.6"]
+            [reifyhealth/lein-git-down "0.4.1"]]
+  :ring {:handler cqrs-web-services.core.endpoints/handler
+         :async? true}
+  :source-paths ["src"]
+  :middleware [lein-git-down.plugin/inject-properties]
+  :repositories [["public-github" {:url "git://github.com"}]]
+  :git-down {ring-sse {:coordinates bobby/ring-sse}}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "1.5.648"]
                  [liberator "0.15.1"]
                  [compojure "1.6.0"]
                  [clj-http "3.12.3"]
-                 ;; [com.fzakaria/slf4j-timbre "0.2"] ;; to avoid wierd ns errors: https://github.com/yogthos/migratus/issues/45
                  [com.appsflyer/ketu "0.6.0"]
+                 [ring-sse/ring-sse "master"]
                  [ring/ring-core "1.6.3"]])
--- cqrs-web-services/src/cqrs_web_services/core/endpoints.clj
+++ cqrs-web-services/src/cqrs_web_services/core/endpoints.clj
@@ -1,31 +1,47 @@
 (ns cqrs-web-services.core.endpoints
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.sse :as sse]
             [compojure.core :refer [defroutes ANY POST GET]]
-            [cqrs-web-services.core.log-connector :refer [add-to-pending!]]
+            [cqrs-web-services.core.log-connector :refer [add-to-pending! <accepted]]
+            [clojure.core.async :as a]
             [clojure.core.async :refer [chan go]]))
 
 
-(defroutes app
-  (ANY "/" [] (resource :available-media-types ["text/html"]
-                        :handle-ok "<html>Hello world</html>"))
-  (ANY "/commands" []
-        (resource
-         :allowed-methods [:post]
-         :available-media-types ["application/edn"]
-         :handle-ok (fn [ctx] (format (pr-str {:response "Command sent"})))
-         :post! (fn [ctx] ;; TODO: move out of handler body
-                  (let [command
-                        (slurp (get-in ctx [:request :body]))]
-                    ;; TODO: validate with schema
-                    (add-to-pending! command)
-                    {:body (str "Command sent: " command)}
-                    )
-                  )
-         )
-        ))
 
-;; (add-to-pending! "do")
+(def test-async-handler
+  (sse/event-channel-handler
+   (fn [request response raise event-ch]
+     (a/pipe <accepted event-ch)
+     )
+   {:on-client-disconnect (fn [] (println "client-disconnect"))}
+   ))
+
+
+(defroutes app
+  (ANY "/" [] (fn [request respond raise]
+                (respond ((resource :available-media-types ["text/html"]
+                                     :handle-ok "<html>Hello world</html>")
+                          request))))
+  (ANY "/commands" []
+       (fn [request respond raise]
+         (respond
+          ((resource
+            :allowed-methods [:post]
+            :available-media-types ["application/edn"]
+            :handle-ok (fn [ctx] (format (pr-str {:response "Command sent"})))
+            :post! (fn [ctx] ;; TODO: move out of handler body
+                     (let [command
+                           (slurp (get-in ctx [:request :body]))]
+                       ;; TODO: validate with schema
+                       (dosync
+                         (println command)
+                         (add-to-pending! (pr-str {:data command})))
+                       )))
+           request))))
+  (GET "/accepted" [] test-async-handler)
+)
+
 (def handler
   (-> app
       wrap-params ;; wraps the app with request params
